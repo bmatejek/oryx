39ef51ba69a996acfa7e17e1cf37b6f0cb9402e4
Brian Matejek <bmatejek@gmail.com>
2019-03-30 01:56:43 -0400

    baselines: added topological thinning

---
+++ baselines/cpp-topological-thinning.h
@@ -0,0 +1,10 @@
+#ifndef __CPP_TOPOLOGICAL_THINNING__
+#define __CPP_TOPOLOGICAL_THINNING__
+
+#include <ctime>
+
+// function calls across cpp files
+void CppTopologicalThinning(const char *prefix, const char *lookup_table_directory, long label);
+
+
+#endif
