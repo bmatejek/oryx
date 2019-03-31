2f3c96175870b6881097ac39f0223d2b73677bb1
Brian Matejek <bmatejek@gmail.com>
2019-03-30 21:24:27 -0400

    transforms: works with JWR and Zebrafinch

--- transforms/volumetric_somae.py
+++ transforms/volumetric_somae.py
@@ -1,3 +1,4 @@
+import os
 import h5py
 import numpy as np
 import struct
@@ -8,19 +9,27 @@ from oryx.utilities import dataIO
 
 
 def CreateVolumetricSomae(prefix, label, subset):
-    if 'JWR':
-        somae_filename = 'raw_data/somae/{}/cell{:03d}_d.h5'.format(prefix, label)
+    somae_filename = 'raw_data/somae/{}/cell{:03d}_d.h5'.format(prefix, label)
+
+    segment_filename = '{}/{}/{:06d}.pts'.format(subset, prefix, label)
+    if not os.path.exists(segment_filename): return
+
+    if prefix == 'JWR':
         downz, downy, downx = 4, 4, 4
+    elif prefix == 'Zebrafinch':
+        downz, downy, downx = 8, 8, 8
 
     # get the grid size
     zres, yres, xres = dataIO.GridSize(prefix)
-    
+
     with h5py.File(somae_filename, 'r') as hf:
         somae = np.array(hf['main'])
 
     # read all segment points
     point_cloud = dataIO.ReadPoints(prefix, label, subset)
     somae_point_cloud = set()
+
+    low_zres, low_yres, low_xres = somae.shape
 
     for index in point_cloud:
         iz = index / (yres * xres)
@@ -30,6 +39,14 @@ def CreateVolumetricSomae(prefix, label, subset):
         zdown = iz / downz
         ydown = iy / downy
         xdown = ix / downx
+
+        # bounds checking
+        if low_zres - 1 < zdown:
+            zdown = low_zres - 1
+        if low_yres - 1 < ydown:
+            ydown = low_yres - 1
+        if low_xres - 1 < xdown:
+            xdown = low_xres - 1
 
         if somae[zdown, ydown, xdown]:
             somae_point_cloud.add(index)
