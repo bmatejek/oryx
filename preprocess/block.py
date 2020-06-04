1621fb36af5409a32a5c43a8934f69c3094ccc9c
Brian Matejek <bmatejek@gmail.com>
2020-06-04 00:26:59 -0400

    block: changed combine blocks to parallel

--- preprocess/block.py
+++ preprocess/block.py
@@ -70,7 +70,7 @@ def GenerateBlocks(prefix, label):
 
 
 
-def CombineBlocks(prefix):
+def CombineBlocks(prefix, iz, iy, ix):
     # get the number of blocks
     zres, yres, xres = GridSize(prefix)
     block_sizes = (1024, 1024, 1024)
@@ -82,37 +82,33 @@ def CombineBlocks(prefix):
     # get the labels for this dataset
     labels = [int(label[:-4]) for label in sorted(os.listdir('segmentations/{}'.format(prefix)))]
 
-    for iz in range(nzblocks):
-        for iy in range(nyblocks):
-            for ix in range(nxblocks):
+    start_time = time.time()
 
-                start_time = time.time()
-                
-                seg = np.zeros((block_sizes[OR_Z], block_sizes[OR_Y], block_sizes[OR_X]), dtype=np.int64)
+    seg = np.zeros((block_sizes[OR_Z], block_sizes[OR_Y], block_sizes[OR_X]), dtype=np.int64)
 
-                for label in labels:
-                    input_filename = 'blocks/sections/{:04d}z-{:04d}y-{:04d}x-{:016d}.pts'.format(iz, iy, ix, label)
-                    if not os.path.exists(input_filename): continue
+    for label in labels:
+        input_filename = 'blocks/sections/{:04d}z-{:04d}y-{:04d}x-{:016d}.pts'.format(iz, iy, ix, label)
+        if not os.path.exists(input_filename): continue
 
-                    # read in the points for this label
-                    with open(input_filename, 'rb') as fd:
-                        input_zres, input_yres, input_xres, input_nelements, = struct.unpack('qqqq', fd.read(32))
-                        assert (input_zres == zres)
-                        assert (input_yres == yres)
-                        assert (input_xres == xres)
+        # read in the points for this label
+        with open(input_filename, 'rb') as fd:
+            input_zres, input_yres, input_xres, input_nelements, = struct.unpack('qqqq', fd.read(32))
+            assert (input_zres == zres)
+            assert (input_yres == yres)
+            assert (input_xres == xres)
 
-                        for _ in range(input_nelements):
-                            index, = struct.unpack('q', fd.read(8))
+            for _ in range(input_nelements):
+                index, = struct.unpack('q', fd.read(8))
 
-                            iw = index // (block_sizes[OR_Y] * block_sizes[OR_X])
-                            iv = (index - iw * block_sizes[OR_Y] * block_sizes[OR_X]) // block_sizes[OR_X]
-                            iu = index % block_sizes[OR_X]
+                iw = index // (block_sizes[OR_Y] * block_sizes[OR_X])
+                iv = (index - iw * block_sizes[OR_Y] * block_sizes[OR_X]) // block_sizes[OR_X]
+                iu = index % block_sizes[OR_X]
 
-                            seg[iw,iv,iu] = label
+                seg[iw,iv,iu] = label
 
-                output_filename = 'raw_segmentations/Fib25/{:04d}x{:04d}x{:04d}/{:04d}z-{:04d}y-{:04d}x.h5'.format(block_sizes[OR_X], block_sizes[OR_Y], block_sizes[OR_Z], iz, iy, ix)
+    output_filename = 'raw_segmentations/Fib25/{:04d}x{:04d}x{:04d}/{:04d}z-{:04d}y-{:04d}x.h5'.format(block_sizes[OR_X], block_sizes[OR_Y], block_sizes[OR_Z], iz, iy, ix)
 
-                with h5py.File(output_filename, 'w') as hf:
-                    hf.create_dataset('main', data=seg, compression='gzip')
+    with h5py.File(output_filename, 'w') as hf:
+        hf.create_dataset('main', data=seg, compression='gzip')
 
-                print ('Finished block {:04d}z-{:04d}y-{:04d}x in {:0.2f} seconds'.format(iz, iy, ix, time.time() - start_time))
+    print ('Finished block {:04d}z-{:04d}y-{:04d}x in {:0.2f} seconds'.format(iz, iy, ix, time.time() - start_time))
