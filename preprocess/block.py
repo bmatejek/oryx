89976945455a7092df1567643f8c9e12fef2d6bd
Brian Matejek <bmatejek@gmail.com>
2020-06-03 23:58:05 -0400

    block: created method for combining blocks

--- preprocess/block.py
+++ preprocess/block.py
@@ -62,3 +62,48 @@ def GenerateBlocks(prefix, label):
                     fd.write(struct.pack('%sq' % npoints, *points_per_block[(iz, iy, ix)]))
 
     print ('Completed label {} in {:0.2f} seconds.'.format(label, time.time() - start_time))
+
+
+
+def CombineBlocks(prefix):
+    # get the number of blocks
+    zres, yres, xres = GridSize(prefix)
+    block_sizes = (1024, 1024, 1024)
+
+    nzblocks = int(math.ceil(float(zres) / block_sizes[OR_Z]))
+    nyblocks = int(math.ceil(float(yres) / block_sizes[OR_Y]))
+    nxblocks = int(math.ceil(float(xres) / block_sizes[OR_X]))
+
+    # get the labels for this dataset
+    labels = [int(label[:-4]) for label in sorted(os.listdir('segmentations/{}'.format(prefix)))]
+
+    for iz in range(nzblocks):
+        for iy in range(nyblocks):
+            for ix in range(nxblocks):
+
+                seg = np.zeros((block_sizes[OR_Z], block_sizes[OR_Y], block_sizes[OR_X]), dtype=np.int64)
+
+                for label in labels:
+                    input_filename = 'blocks/sections/{:04d}z-{:04d}y-{:04d}x-{:016d}.pts'.format(iz, iy, ix, label)
+                    if not input_filename: continue
+
+                    # read in the points for this label
+                    with open(input_filename, 'rb') as fd:
+                        input_zres, input_yres, input_xres, input_nelements, = struct.unpack('qqqq', fd.read(32))
+                        assert (input_zres == block_sizes[OR_Z])
+                        assert (input_yres == block_sizes[OR_Y])
+                        assert (input_xres == block_sizes[OR_X])
+
+                        for _ in range(nelements):
+                            index, = struct.unpack('q', fd.read(8))
+
+                            iz = index // (block_sizes[OR_Y] * block_sizes[OR_X])
+                            iy = (index - iz * block_sizes[OR_Y] * block_sizes[OR_X]) // block_sizes[OR_X]
+                            ix = index % block_sizes[OR_X]
+
+                            seg[iz,iy,ix] = label
+
+                output_filename = 'raw_segmentations/Fib25/{:04d}x{:04d}x{:04d}/{:04d}z-{:04d}y-{:04d}x.h5'.format(block_sizes[OR_X], block_sizes[OR_Y], block_sizes[OR_Z], iz, iy, ix)
+
+                with h5py.File(output_filename, 'w') as hf:
+                    hf.create_dataset('main', data=seg, compression='gzip')
