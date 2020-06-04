b879b8d7223807ad2fc30071553049af6936ec11
bmatejek <bmatejek@gmail.com>
2020-06-04 00:24:33 -0400

    preprocess: fixed h5 output bug

--- preprocess/block.py
+++ preprocess/block.py
@@ -1,7 +1,12 @@
 import os
 import math
 import time
+import h5py
 import struct
+
+
+
+import numpy as np
 
 
 
@@ -54,7 +59,7 @@ def GenerateBlocks(prefix, label):
     for iz in range(nzblocks):
         for iy in range(nyblocks):
             for ix in range(nxblocks):
-                output_filename = 'blocks/sections/{:04d}z-{:04d}y-{:04d}x-{:016d}.h5'.format(iz, iy, ix, label)
+                output_filename = 'blocks/sections/{:04d}z-{:04d}y-{:04d}x-{:016d}.pts'.format(iz, iy, ix, label)
 
                 with open(output_filename, 'wb') as fd:
                     npoints = len(points_per_block[(iz, iy, ix)])
@@ -81,29 +86,33 @@ def CombineBlocks(prefix):
         for iy in range(nyblocks):
             for ix in range(nxblocks):
 
+                start_time = time.time()
+                
                 seg = np.zeros((block_sizes[OR_Z], block_sizes[OR_Y], block_sizes[OR_X]), dtype=np.int64)
 
                 for label in labels:
                     input_filename = 'blocks/sections/{:04d}z-{:04d}y-{:04d}x-{:016d}.pts'.format(iz, iy, ix, label)
-                    if not input_filename: continue
+                    if not os.path.exists(input_filename): continue
 
                     # read in the points for this label
                     with open(input_filename, 'rb') as fd:
                         input_zres, input_yres, input_xres, input_nelements, = struct.unpack('qqqq', fd.read(32))
-                        assert (input_zres == block_sizes[OR_Z])
-                        assert (input_yres == block_sizes[OR_Y])
-                        assert (input_xres == block_sizes[OR_X])
+                        assert (input_zres == zres)
+                        assert (input_yres == yres)
+                        assert (input_xres == xres)
 
-                        for _ in range(nelements):
+                        for _ in range(input_nelements):
                             index, = struct.unpack('q', fd.read(8))
 
-                            iz = index // (block_sizes[OR_Y] * block_sizes[OR_X])
-                            iy = (index - iz * block_sizes[OR_Y] * block_sizes[OR_X]) // block_sizes[OR_X]
-                            ix = index % block_sizes[OR_X]
+                            iw = index // (block_sizes[OR_Y] * block_sizes[OR_X])
+                            iv = (index - iw * block_sizes[OR_Y] * block_sizes[OR_X]) // block_sizes[OR_X]
+                            iu = index % block_sizes[OR_X]
 
-                            seg[iz,iy,ix] = label
+                            seg[iw,iv,iu] = label
 
                 output_filename = 'raw_segmentations/Fib25/{:04d}x{:04d}x{:04d}/{:04d}z-{:04d}y-{:04d}x.h5'.format(block_sizes[OR_X], block_sizes[OR_Y], block_sizes[OR_Z], iz, iy, ix)
 
                 with h5py.File(output_filename, 'w') as hf:
                     hf.create_dataset('main', data=seg, compression='gzip')
+
+                print ('Finished block {:04d}z-{:04d}y-{:04d}x in {:0.2f} seconds'.format(iz, iy, ix, time.time() - start_time))
